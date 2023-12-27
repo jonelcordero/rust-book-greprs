8461b604367d82daa9e4cbb00dfddc4f3cb44274
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2023-12-27 15:45:29 -0800

    6.1 accepting command line arguments

---
+++ target/debug/deps/greprs-2ed928d2a1c22810.d
@@ -0,0 +1,5 @@
+/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-2ed928d2a1c22810: src/main.rs
+
+/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-2ed928d2a1c22810.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/greprs.d
@@ -0,0 +1,1 @@
+/Volumes/mac/RustTextbookProjects/greprs/target/debug/greprs: /Volumes/mac/RustTextbookProjects/greprs/src/main.rs
