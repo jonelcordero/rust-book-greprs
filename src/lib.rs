a0bedb2c5c0bb4a576538ba63bebd9d4ad4f17b5
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2023-12-28 11:01:10 -0800

    1.3 improving with iter

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name")
+        };
 
         let case_sensative = env::var("CASE_INSENSATIVE").is_err();
-    
+        
         Ok(Config { 
             query: query, 
             filename: filename,
