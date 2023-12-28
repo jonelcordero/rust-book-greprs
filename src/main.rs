ca88fd0b5e0b21542f892e91a04a9cf2f2ff535a
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2023-12-27 20:20:42 -0800

    6.6 write to stderr

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@ extern crate greprs;
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 
 use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}", 
+            err
+        ).expect("Could not write to stderr");
         process::exit(1);
     });
 
@@ -17,7 +23,11 @@ fn main() {
     println!("In file {}", config.filename);
     
     if let Err(e) = greprs::run(config) {
-        println!("Application Error: {}", e);
+        writeln!(
+            &mut stderr,
+            "Application Error: {}", 
+            e
+        ).expect("Could not write to stderr");
 
         process::exit(1);
     }
