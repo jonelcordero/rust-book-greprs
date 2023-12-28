400f761f983cf74d53af32e949c9badba8fb4bea
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2023-12-27 16:31:23 -0800

    cargo clean

--- target/debug/deps/greprs-2ed928d2a1c22810.d
+++
@@ -1,5 +0,0 @@
-/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-2ed928d2a1c22810: src/main.rs
-
-/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-2ed928d2a1c22810.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/greprs-85d85272ff3a0180.d
+++
@@ -1,5 +0,0 @@
-/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-85d85272ff3a0180: src/main.rs
-
-/Volumes/mac/RustTextbookProjects/greprs/target/debug/deps/greprs-85d85272ff3a0180.d: src/main.rs
-
-src/main.rs:
